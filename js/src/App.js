7afb2f5a457323b2bed4ccea488b2ef54dce2685
Yosra <nemriyosra1@yahoo.com>
2021-04-30 09:07:02 +0100

    js

---
+++ js/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+import Form from "./Component/Form/Form";
+
+function App() {
+    return (
+        <div className="App">
+            <h1> Welcome to my page </h1>
+            <MyForm />
+            <br />
+            <footer> React Js Checkpoint</footer>
+        </div>
+    );
+}
+
+export default App;
